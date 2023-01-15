60846c4430674c81c6c7f680c91d4b3ed371f347
nephros <nemo@pgxperiiia10>
2023-01-16 00:17:33 +0100

    lets call this first release

--- jolla-settings/pages/peekfilter-button/mainpage.qml
+++ jolla-settings/pages/peekfilter-button/mainpage.qml
@@ -46,14 +46,14 @@ Page { id: page
                 wrapMode: Text.Wrap
                 color: Theme.highlightColor
                 font.pixelSize: Theme.fontSizeSmall
-                text: qsTr("The edge boundary (a.k.a. peek boundary) describes the area at the edge of the screen which is recognized as an edge swipe gesture (as opposed to a swipe within an application window).")
+                text: qsTr("The slider below enbles you to configure the area of the screen which is recognized as an 'Edge Swipe' gesture (as opposed to a swipe within an application window).")
             }
             LinkedLabel {
                 width: parent.width
                 wrapMode: Text.Wrap
                 color: Theme.highlightColor
                 font.pixelSize: Theme.fontSizeSmall
-                plainText: qsTr("Increasing the boundary can help when edge swipes are not recognized reliably. For more information, see https://docs.sailfishos.org/Reference/Sailfish_OS_Tips_and_Tricks/#easing-edge-swipe")
+                plainText: qsTr("Adjusting this can help when edge swipes are not recognized reliably. For more information, see https://docs.sailfishos.org/Reference/Sailfish_OS_Tips_and_Tricks/#easing-edge-swipe")
                 shortenUrl: true
             }
             Item {
--- jolla-settings/translations/jolla-settings-en.ts
+++ jolla-settings/translations/jolla-settings-en.ts
@@ -25,37 +25,37 @@
 <context>
     <name>mainpage</name>
     <message>
-        <location filename="../pages/peekfilter-button/mainpage.qml" line="31"/>
+        <location filename="../pages/peekfilter-button/mainpage.qml" line="29"/>
         <source>Reset to default</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../pages/peekfilter-button/mainpage.qml" line="44"/>
+        <location filename="../pages/peekfilter-button/mainpage.qml" line="42"/>
         <source>Edge Swipe</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../pages/peekfilter-button/mainpage.qml" line="51"/>
-        <source>The edge boundary (a.k.a. peek boundary) describes the area at the edge of the screen which is recognized as an edge swipe gesture (as opposed to a swipe within an application window).</source>
+        <location filename="../pages/peekfilter-button/mainpage.qml" line="49"/>
+        <source>The slider below enbles you to configure the area of the screen which is recognized as an &apos;Edge Swipe&apos; gesture (as opposed to a swipe within an application window).</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../pages/peekfilter-button/mainpage.qml" line="58"/>
-        <source>Increasing the boundary can help when edge swipes are not recognized reliably. For more information, see https://docs.sailfishos.org/Reference/Sailfish_OS_Tips_and_Tricks/#easing-edge-swipe</source>
+        <location filename="../pages/peekfilter-button/mainpage.qml" line="56"/>
+        <source>Adjusting this can help when edge swipes are not recognized reliably. For more information, see https://docs.sailfishos.org/Reference/Sailfish_OS_Tips_and_Tricks/#easing-edge-swipe</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../pages/peekfilter-button/mainpage.qml" line="114"/>
+        <location filename="../pages/peekfilter-button/mainpage.qml" line="112"/>
         <source>Careful: setting this too low will result in you not being able to swipe away applications at all.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../pages/peekfilter-button/mainpage.qml" line="122"/>
+        <location filename="../pages/peekfilter-button/mainpage.qml" line="120"/>
         <source>Hint: For certain applications, like some games, it can be useful to reduce or disable the boundary completely. The most convenient way to do that is to add the Swipe Lock button control to the Top Menu.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../pages/peekfilter-button/mainpage.qml" line="126"/>
+        <location filename="../pages/peekfilter-button/mainpage.qml" line="124"/>
         <source>Open Topmenu Settings</source>
         <translation type="unfinished"></translation>
     </message>
