617e3d51f90739c3dfb5f59550c421ae2be2e7c3
nephros <nemo@pgxperiiia10>
2023-01-16 20:58:24 +0100

    Update German translation

--- jolla-settings/translations/jolla-settings-de.ts
+++ jolla-settings/translations/jolla-settings-de.ts
@@ -37,27 +37,27 @@
     <message>
         <location filename="../pages/peekfilter-button/mainpage.qml" line="49"/>
         <source>The slider below enbles you to configure the area of the screen which is recognized as an &apos;Edge Swipe&apos; gesture (as opposed to a swipe within an application window).</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Schieberegler unten lässt dich den Bereich des Bildschirms festlegen, der als &apos;Edge Swipe&apos;-Geste erkannt wird (im Gegensatz zu einer Geste in einer App). </translation>
     </message>
     <message>
         <location filename="../pages/peekfilter-button/mainpage.qml" line="56"/>
         <source>Adjusting this can help when edge swipes are not recognized reliably. For more information, see https://docs.sailfishos.org/Reference/Sailfish_OS_Tips_and_Tricks/#easing-edge-swipe</source>
-        <translation type="unfinished"></translation>
+        <translation>Das zu verändern kann helfen, wenn diese Gesten nicht zuverläsig erkannt werden. Für mehr Informationen siehe https://docs.sailfishos.org/Reference/Sailfish_OS_Tips_and_Tricks/#easing-edge-swipe</translation>
     </message>
     <message>
         <location filename="../pages/peekfilter-button/mainpage.qml" line="112"/>
         <source>Careful: setting this too low will result in you not being able to swipe away applications at all.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vorsicht: wenn du den Wert zu klein setzt wirst du Apps nicht mehr seitlich wegwischen können.</translation>
     </message>
     <message>
         <location filename="../pages/peekfilter-button/mainpage.qml" line="120"/>
         <source>Hint: For certain applications, like some games, it can be useful to reduce or disable the boundary completely. The most convenient way to do that is to add the Swipe Lock button control to the Top Menu.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tipp: für manche Apps (z.B. Spiele) kann es hilfreich sein, den Edge Swipe ganz auszuschalten. Am besten geht das, indem du den &apos;Swipe Lock&apos; Knopf zum Oberen Menü hinzufügst.</translation>
     </message>
     <message>
         <location filename="../pages/peekfilter-button/mainpage.qml" line="124"/>
         <source>Open Topmenu Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen zum Oberen Menü öffnen</translation>
     </message>
 </context>
 </TS>
