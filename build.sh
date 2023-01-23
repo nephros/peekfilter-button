7607ceee332e3d800dc2890c34876fe236909f03
nephros <nemo@pgxperiiia10>
2023-01-23 11:58:24 +0100

    clean up qm files when building locally

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 specify -Nns rpm/*yaml || exit 1
 printf building...
+rm jolla-settings/translations/*.qm
 rpmbuild -bb --build-in-place rpm/*.spec > build.log 2>&1
 printf "exit: $?\n"
 grep ^Wrote build.log
